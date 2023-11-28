144144bd742119bff1ce1c67cce12ba37ccc058b
Nikolay <kolunya0@gmail.com>
2023-11-28 18:34:37 +0300

    task 3 rev2.0

--- mole-game/task.js
+++ mole-game/task.js
@@ -6,29 +6,22 @@ const holes = document.querySelectorAll(".hole");
 let score = 0;
 let misses = 0;
 
-// Функция для обработки клика по лунке с кротом
-function handleMoleClick() {
-  // Увеличиваем счетчик убитых кротов
-  score++;
-  deadCount.textContent = score;
+// Функция для обработки клика по ячейке
+function handleClick() {
+  if (this.classList.contains("hole_has-mole")) {
+    // Если это ячейка с кротом, увеличиваем счетчик убитых кротов
+    score++;
+    deadCount.textContent = score;
 
-  // Удаляем обработчик клика, чтобы крот не мог быть убит дважды
-  this.removeEventListener("click", handleMoleClick);
-}
-
-// Функция для обработки клика по пустой лунке
-function handleMiss() {
-  // Увеличиваем счетчик промахов
-  misses++;
-  lostCount.textContent = misses;
-}
-
-// Добавляем обработчики клика для каждой лунки
-holes.forEach((hole) => {
-  // Если лунка имеет класс "hole_has-mole", значит, это лунка с кротом
-  if (hole.classList.contains("hole_has-mole")) {
-    hole.addEventListener("click", handleMoleClick);
-  } else {
-    hole.addEventListener("click", handleMiss);
+  } else if (!this.classList.contains("hole_has-mole")){
+    // Если это пустая ячейка, увеличиваем счетчик промахов
+    misses++;
+    lostCount.textContent = misses;
   }
+}
+
+// Добавляем обработчики клика для каждой ячейки
+holes.forEach((hole) => {
+  hole.addEventListener("click", handleClick);
+  this.removeEventListener("click", handleClick);
 });
