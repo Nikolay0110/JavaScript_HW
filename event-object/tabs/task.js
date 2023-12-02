9ae48e18e4872b6cce1913af9ee2cd74e37839d2
Nikolay <kolunya0@gmail.com>
2023-12-02 21:52:07 +0300

    task 3 rev1

---
+++ event-object/tabs/task.js
@@ -0,0 +1,24 @@
+const element = document.querySelector('.tab')
+const list = element.querySelector('.tab__content ')
+
+addEventListener('click', element.onClick = () => {
+    element.classList.add('active_tab')
+})
+
+
+
+
+
+
+
+//
+// function activeClick (e) {
+//     element.onclick = function (e) {
+//         e.classList.add('tab_active');
+//     }
+//     list.onclick = function (e) {
+//         e.classList.add('tab__content_active');
+//     }
+//     e.addEventListener('click', activeClick)
+// }
+
