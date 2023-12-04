c401983d98bf284581d97da8a0a7065452d41615
Nikolay <kolunya0@gmail.com>
2023-12-05 00:22:32 +0300

    task 3 add comments

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,15 @@
-const arr1 = Array.from(document.querySelectorAll('.tab'));
-const arr2 = Array.from(document.querySelectorAll('.tab__content'));
-const scan = document.querySelector('.tab__navigation');
+const arr1 = Array.from(document.querySelectorAll('.tab')); // забираем массив тегов объектов клика
+const arr2 = Array.from(document.querySelectorAll('.tab__content')); // забираем массив с тегами содержимого
+const scan = document.querySelector('.tab__navigation'); // объект сканирвания
 
-scan.addEventListener('click', (event) => {
-    const def = arr1.indexOf(event.target);
-    console.log(def);
-    for (let i = 0; i < arr1.length; i++) {
-        if (i !== def) {
-            arr1[i].classList.remove('tab_active');
-            arr2[i].classList.remove('tab__content_active');
+scan.addEventListener('click', (event) => { // вызываем обработчик на клик в который передаем event
+    const def = arr1.indexOf(event.target); // получаем индес элеменат массива по которому был клик
+    for (let i = 0; i < arr1.length; i++) { // запускаем цикл для удаления классов отображения информации если по элементу не было клика
+        if (i !== def) { // и если это так то
+            arr1[i].classList.remove('tab_active'); // удаляем класс активного состояния
+            arr2[i].classList.remove('tab__content_active'); // удаляем класс отображения содержимого
         }
-        arr1[def].classList.add('tab_active');
-        arr2[def].classList.add('tab__content_active');
+        arr1[def].classList.add('tab_active'); // а элементу по которому кликнули присваиваем необхлжимый класс
+        arr2[def].classList.add('tab__content_active'); // и соответственно тегу с контентом присваиваем класс отображения контента по тому же индексу, так как они совпадают на всей странице
     }
 });
