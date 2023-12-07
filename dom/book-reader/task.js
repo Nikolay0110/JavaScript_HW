d197651cfd513f33991d7ab19a29835581481dbd
Nikolay <kolunya0@gmail.com>
2023-12-07 22:33:27 +0300

    task 3 complited

---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const control = Array.from(document.querySelectorAll('.font-size')); // массив
+const scan = document.querySelector('.book__control'); // объект сканирования
+const book = document.querySelector('.book');
+
+scan.addEventListener('click', (event) => { // обработчик клика
+    const def = control.indexOf(event.target);// индекс элемента массива по которому был клик
+    event.preventDefault(); // отключение обновления страницы после клика
+    for (let i = 0; i < control.length; i++) { // пробегаемся циклом по индексам массива
+        if (i !== def) control[i].classList.remove('font-size_active'); // удаляем активноый размер если он не равен индксу клика
+        control[def].classList.add('font-size_active'); // назначаем активный шрифт равный элементу клика
+        if (def === 0) { // переключение шрифта у объекта с текстом - book
+            book.classList.add('font-size_small') // если индекс клика равен 0 то small
+        } else if (def === 2) {
+            book.classList.add('font-size_big') // если индекс клика равен 2 то big
+        } else { // в ином случае удаляем изменения шрифта и ставим дефолтное значение
+            book.classList.remove('font-size_small') // удаление селектора уменьшенного шрифта
+            book.classList.remove('font-size_big') // удаление селектора увеличенного шрифта
+        }
+    }
+});
