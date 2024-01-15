f839edf365c08c75cf967411daca16c70f4b0da4
Nikolay <kolunya0@gmail.com>
2024-01-15 20:33:54 +0300

    remove task 7

--- modules/src/js/app.js
+++
@@ -1,5 +0,0 @@
-console.log('app worked')
-
-
-const game = new Game();
-game.start();
--- modules/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    output: {
-        path: '/home/nikolay/Projects/JavaScript_HW/dist/',
-        filename: 'main.js'
-    }
-}
